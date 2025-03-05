05566bb3c8e9da19d5e2e3d7f4add5a12e2ee274
Mahdi <mahdiwise@gmail.com>
2025-03-05 16:49:24 +0330

    change sh file

--- start.sh
+++ start.sh
@@ -4,17 +4,13 @@
 TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
-# Serve the API and don't shutdown the container
-if [ "$SERVE_API_LOCALLY" == "true" ]; then
-    echo "rvcv2-runpod: Starting RVC-v2-UI"
-    python3 /RVC-v2-UI/src/webui.py &
+# Start RVC-v2-UI
+echo "rvcv2-runpod: Starting RVC-v2-UI"
+python3 /RVC-v2-UI/src/webui.py &
 
-    echo "rvcv2-runpod: Starting Runpod Handler"
-    python3 -u /rp_handler.py &
-else
-    echo "rvcv2-runpod: Starting RVC-v2-UI"
-    python3 /RVC-v2-UI/src/webui.py &
+# Start Runpod Handler
+echo "rvcv2-runpod: Starting Runpod Handler"
+python3 -u /rp_handler.py &
 
-   echo "rvcv2-runpod: Starting Runpod Handler"
-    python3 -u /rp_handler.py &
-fi
+# Keep the script running until all background processes are done
+wait
