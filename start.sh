41c1349a3a13c8ceabffe7c88aaaecea1cf8239d
Mahdi <mahdiwise@gmail.com>
2025-03-05 16:46:36 +0330

    bruh in start file

--- start.sh
+++ start.sh
@@ -7,14 +7,14 @@ export LD_PRELOAD="${TCMALLOC}"
 # Serve the API and don't shutdown the container
 if [ "$SERVE_API_LOCALLY" == "true" ]; then
     echo "rvcv2-runpod: Starting RVC-v2-UI"
-    python3 /RVC-v2-UI/src/webui.py
+    python3 /RVC-v2-UI/src/webui.py &
 
     echo "rvcv2-runpod: Starting Runpod Handler"
-    python3 -u /rp_handler.py
+    python3 -u /rp_handler.py &
 else
     echo "rvcv2-runpod: Starting RVC-v2-UI"
-    python3 /RVC-v2-UI/src/webui.py
+    python3 /RVC-v2-UI/src/webui.py &
 
    echo "rvcv2-runpod: Starting Runpod Handler"
-    python3 -u /rp_handler.py
+    python3 -u /rp_handler.py &
 fi
